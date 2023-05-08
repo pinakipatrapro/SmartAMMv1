af82005340fea5852653baa0b457ca0e77983537
Pinaki Patra <pinaki.patra@t-systems.com>
2023-05-08 16:25:44 +0530

    Add Bar Chart Handler

--- srv/services/charts/Bar.js
+++ srv/services/charts/Bar.js
@@ -1,16 +1,19 @@
 const { PrismaClient, Prisma } = require('@prisma/client');
 const prisma = new PrismaClient();
 const Common = require('./Common')
-class Bar extends Common{
+class Bar extends Common {
 
-    async getData(payload){
-        const viewName = await this.getViewName(payload.projectID)
-        let dimensionString =this.getDimensionString([...payload.dimension,...payload.series])
-        let measureString = this.getMeasureString(payload.measure,payload.agg)
-        let sqlString = 
-            ` SELECT 
+  async getData(payload) {
+    const viewName = await this.getViewName(payload.projectID)
+    if (!payload.series) {
+      payload.series = []
+    }
+    let dimensionString = this.getDimensionString([...payload.dimension, ...payload.series])
+    let measureString = this.getMeasureString(payload.measure, payload.agg)
+    let sqlString =
+      ` SELECT 
                 ${dimensionString}
-                ${payload.measure.length && payload.dimension.length  ? ' , ':''}
+                ${payload.measure.length && payload.dimension.length ? ' , ' : ''}
                 ${measureString}
               FROM
                 (${this.getSqlString(viewName)}) AS V
@@ -18,9 +21,9 @@ class Bar extends Common{
                 ${this.getOrderByClauseString()}
               GROUP BY ${dimensionString}
             `;
-        let  data = await prisma.$queryRawUnsafe( `${sqlString}`);
-        data = this.toObject(data)
-        return data;
-    }
+    let data = await prisma.$queryRawUnsafe(`${sqlString}`);
+    data = this.toObject(data)
+    return data;
+  }
 }
 module.exports = Bar;
