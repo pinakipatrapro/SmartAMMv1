91948e76f6fd8d5fab6db1dd18f38b7ddf1a72ed
Pinaki Patra <pinaki.patra@t-systems.com>
2023-05-12 12:26:00 +0530

    Changes

--- app/src/pages/ProjectsEdit.jsx
+++ app/src/pages/ProjectsEdit.jsx
@@ -1,14 +1,14 @@
 
 import React, { useState, useEffect } from "react";
-import { Typography, Button, Divider, Input, Statistic, Space, Card, notification, Col, Row } from 'antd';
-import { PlusOutlined, EditOutlined, DeleteOutlined, LineChartOutlined } from '@ant-design/icons';
+import { Typography, Button, Divider, Input, Statistic, Space, Card, notification,Form, Col, Row } from 'antd';
+import { PlusOutlined, EditOutlined, DeleteOutlined, LineChartOutlined, SaveOutlined } from '@ant-design/icons';
 import { Table } from 'antd';
 import constant from "../constants/MockData.json"
 import { BrowserRouter, Routes, Route, useParams, useNavigate } from "react-router-dom";
 import millify from "millify";
 import axios from "axios"
 import ColumnCardsViewOnly from '../components/createProject/ColumnCardsViewOnly'
-import CalulatedColOptions from "../components/createProject/CalculatedColOptions";
+import CalculatedColOptionsEdit from "../components/createProject/CalculatedColOptionsEdit";
 
 const { Search } = Input;
 const { Title } = Typography;
@@ -19,7 +19,14 @@ const { Title } = Typography;
 const ProjectEdit = () => {
     const route = useParams()
     const [projectDetails, setProjectDetails] = useState(null)
-    const setCalculatedColumns=(e)=>{
+    const setCalculatedColumns = (e) => {
+        let projectData = JSON.parse(JSON.stringify(projectDetails));
+        projectData.calculatedColumns = e;
+        setProjectDetails(projectData)
+
+    }
+
+    const saveCalculatedColumns=(e)=>{
         debugger;
     }
     const fetchProjectDetails = async () => {
@@ -42,7 +49,9 @@ const ProjectEdit = () => {
         <>
             <Card title={projectDetails.name}
                 extra={
-                    <Space>Total Records : <Statistic value={projectDetails.rowsAnalysed} /></Space>
+                    <Space>Total Records : <Statistic value={projectDetails.rowsAnalysed} />
+                        
+                    </Space>
                 }
             >
                 <Typography.Paragraph>Description : {projectDetails.description}</Typography.Paragraph>
@@ -52,16 +61,19 @@ const ProjectEdit = () => {
 
 
             <Divider style={{ padding: "0rem 1rem 0 1rem" }}> Define Calculated Columns</Divider>
-            <CalulatedColOptions setCalculatedColumns={setCalculatedColumns} calculatedColumns={projectDetails.calculatedColumns}
-                previewData={projectDetails.configData}
-            />
+            <Form id="calculatedColumn" onFinish={saveCalculatedColumns}>
+                
+                <CalculatedColOptionsEdit setCalculatedColumns={setCalculatedColumns} calculatedColumns={projectDetails.calculatedColumns}
+                    previewData={projectDetails.configData}
+                />
+            </Form>
 
 
 
 
 
             <Divider>Columns ({projectDetails.configData.length})</Divider>
-            <Typography.Paragraph style={{textAlign:"center"}} type="secondary">Editing columns in edit mode is not currently available. Please create a new project to re-define the columns</Typography.Paragraph>
+            <Typography.Paragraph style={{ textAlign: "center" }} type="secondary">Editing columns in edit mode is not currently available. Please create a new project to re-define the columns</Typography.Paragraph>
             <Row gutter={[8, 8]} style={{ margin: "1rem" }}>
                 {projectDetails.configData.map((e, i) => {
                     return (
